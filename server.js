388ab712a3ffac20fa61ab446419b16d5456ccd9
DangAnhDung <107111135+Dung02052002@users.noreply.github.com>
2024-05-30 19:45:42 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const mysql = require('mysql');
-const cors = require('cors');
+import express from 'express';
+import mysql from 'mysql';
+import cors from 'cors';
+import jwt from 'jsonwebtoken';
+
 
 const app = express();
 app.use(cors());
@@ -15,8 +17,11 @@ const connection = mysql.createConnection({
 
 app.post('/signup', (req, res) => {
     const { name, email, password } = req.body;
-    const sql = "INSERT INTO `login`(`name`, `email`, `password`) VALUES (?, ?, ?)";
-    const values = [name, email, password];
+
+    // tao token
+    const token = jwt.sign({email}, 'your_secret_token');
+    const sql = "INSERT INTO `login`(`name`, `email`, `password`,`token`) VALUES (?, ?, ?, ?)";
+    const values = [name, email, password, token];
 
     connection.query(sql, values, (err, result) => {
         if (err) {
@@ -24,14 +29,14 @@ app.post('/signup', (req, res) => {
             return res.status(500).json({ error: 'Error inserting data into the database' });
         }
 
-        return res.status(200).json({ message: 'Signup successful' });
+        return res.status(200).json({ message: 'Signup successful', token: token });
     });
 });
 
 app.post('/login', (req, res) => {
     const sql = "SELECT * FROM login WHERE `email` = ? AND `password` = ?";
     connection.query(sql, [req.body.email, req.body.password], (err, result) => {
-        if (err) {
+        if (err) {  
             console.error(err);
             return res.status(500).json({ error: 'Error' });
         }
@@ -39,8 +44,12 @@ app.post('/login', (req, res) => {
         if (result.length === 0) {
           return res.status(401).json({ error: 'Invalid email or password' });
         }
+
+        // tao token jwt
+        const playload = {email: result[0].email}
+        const token = jwt.sign(playload, 'your_secret_token');
   
-        return res.status(200).json({ message: 'Login successful' });
+        return res.status(200).json({ message: 'Login successful' + {token: token} });
     });
   });
 
